75b9042859df33fd22684859e867026e7c450d5d
Ravuri Sai Kiran <saikiranravuri786@gmail.com>
2026-08-11 17:28:11 +0530

    Add reset-password

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home.jsx';
 import Login from '../pages/Login.jsx';
 import SignUp from '../pages/SignUp.jsx';
+import ResetPassword from '../pages/ResetPassword.jsx';
 
 function AppRoutes() {
   return (
@@ -10,10 +11,12 @@ function AppRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
     </Routes>
   );
 }
 
 export default AppRoutes;
+
 
 
